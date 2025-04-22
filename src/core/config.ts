0e48ee65853f25b9b1f6afe89e7129dee75e377b
snowden.xu <snowden.xu@gmail.com>
2025-04-22 20:52:12 +0800

    refactor(interceptors): 优化拦截器配置，增强错误处理逻辑

--- src/core/config.ts
+++ src/core/config.ts
@@ -2,6 +2,7 @@ import { RequestConfig } from "@/types";
 
 const defaultConfig: RequestConfig = {
   baseURL: "/",
+  timeout: 10 * 1000,
   interceptors: {
     // 请求拦截器
     requestInterceptor: (config) => {
@@ -34,6 +35,29 @@ const defaultConfig: RequestConfig = {
     // 响应错误拦截器
     responseInterceptorCatch: (error) => {
       // 处理响应错误
+      if (error.response) {
+        const { status, data } = error.response;
+        switch (status) {
+          case 400:
+            console.error("请求错误");
+            break;
+          case 401:
+            console.error("未授权，跳转登录");
+            break;
+          case 403:
+            console.error("没有权限访问资源");
+            break;
+          case 500:
+            console.error("服务器内部错误");
+            break;
+          default:
+            console.error("请求失败");
+        }
+      } else if (error.request) {
+        console.error("请求无响应：", error.message);
+      } else {
+        console.error("请求出错：", error.message);
+      }
       // console.error("Response Error:", error);
       return Promise.reject(error);
     },
--- src/core/interceptors.ts
+++ src/core/interceptors.ts
@@ -1,24 +1,17 @@
-import type { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from "axios";
-import { handleError } from "@/utils/errorHandler";
+import type { AxiosInstance, AxiosResponse } from "axios";
 import type { InterceptorOptions } from "@/types";
 
-export function setInterceptors(instance: AxiosInstance, interceptors?: InterceptorOptions) {
+export function setInterceptors(instance: AxiosInstance, interceptors: InterceptorOptions) {
   // 请求拦截器
-  instance.interceptors.request.use(interceptors?.requestInterceptor || ((config: InternalAxiosRequestConfig) => config), interceptors?.requestInterceptorCatch);
+  instance.interceptors.request.use(interceptors.requestInterceptor, interceptors.requestInterceptorCatch);
 
   // 响应拦截器
   instance.interceptors.response.use(
     async <T>(res: AxiosResponse<T>) => {
-      if (interceptors?.responseInterceptor) {
-        return interceptors.responseInterceptor(res);
-      }
-      return res;
+      return interceptors.responseInterceptor(res);
     },
     (err) => {
-      if (interceptors?.responseInterceptorCatch) {
-        return interceptors.responseInterceptorCatch(err);
-      }
-      return handleError(err);
+      return interceptors.responseInterceptorCatch(err);
     }
   );
 }
